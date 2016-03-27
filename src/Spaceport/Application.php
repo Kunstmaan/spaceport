cba8ef2818a0518a36b01a56c22ba37be35c9bcc
Roderik van der Veer <roderik.van.der.veer@kunstmaan.be>
2016-03-27 14:55:10 +0200

    Cleanup

--- src/Spaceport/Application.php
+++ src/Spaceport/Application.php
@@ -2,8 +2,6 @@
 
 namespace Spaceport;
 
-use Humbug\SelfUpdate\Strategy\GithubStrategy;
-use Humbug\SelfUpdate\Updater;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Application as ConsoleApplication;
@@ -18,51 +16,7 @@ class Application extends ConsoleApplication
 
     public function doRun(InputInterface $input, OutputInterface $output)
     {
-
-        //$this->checkForUpdate();
-
-//        if ($this->isInPharMode() && in_array($commandName, array('new', 'demo'), true)) {
-//            if (!$this->checkIfInstallerIsUpdated()) {
-//                $output->writeln(sprintf(
-//                    " <comment>[WARNING]</comment> Your Symfony Installer version is outdated.\n".
-//                    ' Execute the command "%s selfupdate" to get the latest version.',
-//                    $_SERVER['PHP_SELF']
-//                ));
-//            }
-//        }
-
         return parent::doRun($input, $output);
     }
 
-    public function isInPharMode()
-    {
-        return 'phar://' === substr(__DIR__, 0, 7);
-    }
-
-    private function checkForUpdate()
-    {
-        $updater = new Updater();
-        $updater->setStrategy(Updater::STRATEGY_GITHUB);
-        /** @var GithubStrategy $strategyInterface */
-        $strategyInterface = $updater->getStrategy();
-        $strategyInterface->setPackageName('kunstmaan/spaceport');
-        $strategyInterface->setPharName('spaceport.phar');
-        $strategyInterface->setCurrentLocalVersion($this->getVersion());
-
-        try {
-            $result = $updater->hasUpdate();
-            if ($result) {
-                echo(sprintf(
-                    'The current stable build available remotely is: %s',
-                    $updater->getNewVersion()
-                ));
-            } elseif (false === $updater->getNewVersion()) {
-                echo('There are no stable builds available.');
-            } else {
-                echo('You have the current stable build installed.');
-            }
-        } catch (\Exception $e) {
-            exit('Well, something happened! Either an oopsie or something involving hackers.');
-        }
-    }
 }
