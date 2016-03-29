d77af5e1bcb4e1ea5fcb67071eb83af5956ae672
Roderik van der Veer <roderik.van.der.veer@kunstmaan.be>
2016-03-29 17:59:03 +0200

    Generate the docker-compose.yml file

--- src/Spaceport/Commands/RunCommand.php
+++ src/Spaceport/Commands/RunCommand.php
@@ -1,6 +1,7 @@
 <?php
 namespace Spaceport\Commands;
 
+use Spaceport\Exceptions\NotAJenkinsBuiltProject;
 use Spaceport\Exceptions\NotASymfonyProjectException;
 use Spaceport\Traits\IOTrait;
 use Spaceport\Traits\TwigTrait;
@@ -15,8 +16,7 @@ class RunCommand extends AbstractCommand
     {
         $this
             ->setName('run')
-            ->setDescription('Run the development environment')
-        ;
+            ->setDescription('Run the development environment');
     }
 
     /**
@@ -29,26 +29,54 @@ class RunCommand extends AbstractCommand
     protected function doExecute(InputInterface $input, OutputInterface $output)
     {
         $dockerComposeFileName = 'docker-compose.yml';
-        if (!file_exists($dockerComposeFileName)){
-            $mysql = $this->findMySQLSettings();
+        #if (!file_exists($dockerComposeFileName)) {
+            $variables = array_merge($this->findMySQLSettings(), $this->findApacheSettings(), $this->findPHPSettings());
+            $this->twig->renderAndWriteTemplate('symfony/' . $dockerComposeFileName . '.twig', $dockerComposeFileName, $variables);
+        #}
+    }
 
-            $this->twig->renderAndWriteTemplate('symfony/'.$dockerComposeFileName, $dockerComposeFileName);
+    private function findPHPSettings(){
+        $php = array();
+        $php['php_version'] = '7.0';
+        return $php;
+    }
+
+
+    /**
+     * @return array
+     * @throws NotAJenkinsBuiltProject
+     */
+    private function findApacheSettings()
+    {
+        $apache = array();
+        $apache['apache_vhost'] = basename(getcwd()) . '.docker';
+        $apache['apache_webroot'] = "web/";
+
+        $jenkinsFile = '.skylab/jenkins.yml';
+        if (file_exists($jenkinsFile)) {
+            $yaml = new Parser();
+            $value = $yaml->parse(file_get_contents($jenkinsFile));
+            $apache['apache_fallbackdomain'] = basename(getcwd()) . '.' . $value["deploy_matrix"]["production"]["server"];
+        } else {
+            throw new NotAJenkinsBuiltProject("No jenkins.yml file found at " . $jenkinsFile);
         }
+        return $apache;
     }
 
     /**
      * @return array
      * @throws NotASymfonyProjectException
      */
-    private function findMySQLSettings(){
+    private function findMySQLSettings()
+    {
         $parametersFile = 'app/config/parameters.yml';
         if (file_exists($parametersFile)) {
             $yaml = new Parser();
             $value = $yaml->parse(file_get_contents($parametersFile));
             $mysql = array();
-            $mysql['mysql.database'] = $value["parameters"]["database_name"];
-            $mysql['mysql.user'] = $value["parameters"]["database_user"];
-            $mysql['mysql.password'] = $value["parameters"]["database_password"];
+            $mysql['mysql_database'] = $value["parameters"]["database_name"];
+            $mysql['mysql_user'] = $value["parameters"]["database_user"];
+            $mysql['mysql_password'] = $value["parameters"]["database_password"];
             return $mysql;
         } else {
             throw new NotASymfonyProjectException("No parameters.yml file found at " . $parametersFile);
---
+++ src/Spaceport/Exceptions/NotAJenkinsBuiltProject.php
@@ -0,0 +1,10 @@
+<?php
+namespace Spaceport\Exceptions;
+
+
+use Symfony\Component\Console\Exception\RuntimeException;
+
+class NotAJenkinsBuiltProject extends RuntimeException
+{
+
+}
