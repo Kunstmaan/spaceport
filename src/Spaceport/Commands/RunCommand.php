b67eb940d13b539f69269d79bf7f0c3f21d8d52f
Roderik van der Veer <roderik.van.der.veer@kunstmaan.be>
2016-03-29 18:23:18 +0200

    More check and files

--- src/Spaceport/Commands/RunCommand.php
+++ src/Spaceport/Commands/RunCommand.php
@@ -28,14 +28,65 @@ class RunCommand extends AbstractCommand
      */
     protected function doExecute(InputInterface $input, OutputInterface $output)
     {
+        $this->writeDockerComposeFile();
+        $this->checkAppFile();
+        $this->checkAppKernelFile();
+        $this->writeConfigDockerFile();
+        $this->io->newLine();
+    }
+
+    private function writeConfigDockerFile()
+    {
+        $this->io->text(' - Generating the app/config/config_docker.yml file');
+        $configDockerFileName = 'app/config/config_docker.yml';
+        #if (!file_exists($configDockerFileName)) {
+        $this->twig->renderAndWriteTemplate('symfony/config_docker.yml.twig', $configDockerFileName);
+        #}
+
+    }
+
+    private function checkAppFile()
+    {
+        $this->io->text(' - Checking if the web/app.php file is setup for Docker');
+        if (strpos(file_get_contents("web/app.php"), 'docker') === false) {
+            $this->io->block(
+                'The web/app.php file is not setup for Docker. Change the section where the AppKernel is loaded to look like this:
+                
+if (getenv(\'APP_ENV\') === \'dev\' || getenv(\'APP_ENV\') === \'docker\') {
+    umask(0000);
+    Debug::enable();
+    $kernel = new AppKernel(getenv(\'APP_ENV\'), true);
+} else {
+   $kernel = new AppKernel(\'prod\', false);
+}'
+            );
+        }
+    }
+
+    private function checkAppKernelFile()
+    {
+        $this->io->text(' - Checking if the app/AppKernel.php file is setup for Docker');
+        if (strpos(file_get_contents("app/AppKernel.php"), 'docker') === false) {
+            $this->io->block(
+                'The app/AppKernel.php file is not setup for Docker. Change the section where the dev and test bundles are loaded to look like this:
+                
+if (in_array($this->getEnvironment(), array(\'dev\', \'test\', \'docker\'), true)) {'
+            );
+        }
+    }
+
+    private function writeDockerComposeFile()
+    {
+        $this->io->text(' - Generating the docker-compose.yml file');
         $dockerComposeFileName = 'docker-compose.yml';
         #if (!file_exists($dockerComposeFileName)) {
-            $variables = array_merge($this->findMySQLSettings(), $this->findApacheSettings(), $this->findPHPSettings());
-            $this->twig->renderAndWriteTemplate('symfony/' . $dockerComposeFileName . '.twig', $dockerComposeFileName, $variables);
+        $variables = array_merge($this->findMySQLSettings(), $this->findApacheSettings(), $this->findPHPSettings());
+        $this->twig->renderAndWriteTemplate('symfony/' . $dockerComposeFileName . '.twig', $dockerComposeFileName, $variables);
         #}
     }
 
-    private function findPHPSettings(){
+    private function findPHPSettings()
+    {
         $php = array();
         $php['php_version'] = '7.0';
         return $php;
