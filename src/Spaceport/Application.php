5d6f2711b1f0f4b0453ae10b94a4821105923619
Kevin Jossart <kevin.jossart@kunstmaan.be>
2017-12-01 10:23:34 +0100

    Simple command to install potential future devtools to a configurable directory

--- src/Spaceport/Application.php
+++ src/Spaceport/Application.php
@@ -2,19 +2,14 @@
 
 namespace Spaceport;
 
-use Spaceport\Helpers\TwigHelper;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Application as ConsoleApplication;
 
 class Application extends ConsoleApplication
 {
-    
     public function doRun(InputInterface $input, OutputInterface $output)
     {
         return parent::doRun($input, $output);
     }
-
-
-
 }
---
+++ src/Spaceport/Commands/InstallDevToolsCommand.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Spaceport\Commands;
+
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class InstallDevToolsCommand extends AbstractCommand
+{
+    protected function configure()
+    {
+        $this
+            ->setName('install-dev')
+            ->addArgument('install-dir', InputArgument::OPTIONAL, 'Configure the install dir for the required files')
+            ->setDescription('Install all the development tools to interface with container');
+    }
+
+    protected function doExecute(InputInterface $input, OutputInterface $output)
+    {
+        $ourDir = (realpath(dirname(__FILE__)) . '/../Resources/DevTools');
+        $installDir = '/usr/local/bin/';
+        if ($input->getArgument('install-dir')) {
+            $installDir = $input->getArgument('install-dir');
+        }
+
+        $output->writeln(sprintf('Writing our tools to directory: %s', realpath($installDir)));
+        $command = sprintf("cp %s/* %s/", realpath($ourDir), realpath($installDir));
+
+        if (!is_writable($installDir)) {
+            $command = 'sudo -s -p "Please enter your sudo password:" ' . $command;
+        }
+
+        exec($command);
+    }
+}
