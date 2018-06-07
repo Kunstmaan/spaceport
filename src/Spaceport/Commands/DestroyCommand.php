ae779e5760a50c5bd140b449bcd499d2b0028b27
Ruud Denivel <ruud.denivel@kunstmaan.be>
2018-06-07 11:11:05 +0200

    cleanup

--- src/Spaceport/Commands/DestroyCommand.php
+++ src/Spaceport/Commands/DestroyCommand.php
@@ -3,7 +3,6 @@
 namespace Spaceport\Commands;
 
 use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Process\Process;
 
