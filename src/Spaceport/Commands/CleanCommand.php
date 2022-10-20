70c39ea3af7ae0456172f0205ce583b9a920c5dd
Merel Jossart <merel.jossart@kunstmaan.be>
2022-10-20 10:27:45 +0200

    Allow only deleting of mysql volume

--- src/Spaceport/Commands/CleanCommand.php
+++ src/Spaceport/Commands/CleanCommand.php
@@ -14,6 +14,7 @@ class CleanCommand extends AbstractCommand
             ->setName('clean')
             ->addOption('images', 'i', InputOption::VALUE_NONE, 'Remove images')
             ->addOption('volumes', 'o', InputOption::VALUE_NONE, 'Remove volumes')
+            ->addOption('mysql', 'm', InputOption::VALUE_NONE, 'Remove mysql')
             ->addOption('all', 'a', InputOption::VALUE_NONE, 'Remove everything')
             ->setDescription('Clean docker containers, images and volumes')
         ;
@@ -30,27 +31,24 @@ class CleanCommand extends AbstractCommand
 
         $removeImages = $input->getOption('images');
         $removeVolumes = $input->getOption('volumes');
+        $removeMysql = $input->getOption('mysql');
         $removeAll = $input->getOption('all');
-        if ($removeAll) {
-            $outputString = $this->runCommand('docker image ls -a -q', null, [], true);
-            if (!empty($outputString)) {
-                $this->runCommand('docker image rm $(docker image ls -a -q)');
-            }
 
-            $outputString = $this->runCommand('docker volume ls -q', null, [], true);
+        if($removeMysql) {
+            $outputString = $this->runCommand('docker volume ls -q | grep mysql', null, [], true);
             if (!empty($outputString)) {
-                $this->runCommand('docker volume rm $(docker volume ls -q)');
+                $this->runCommand('docker volume rm $(docker volume ls -q | grep mysql)');
             }
         }
 
-        if (!$removeAll && $removeImages) {
+        if ($removeAll || $removeImages) {
             $outputString = $this->runCommand('docker image ls -a -q', null, [], true);
             if (!empty($outputString)) {
                 $this->runCommand('docker image rm $(docker image ls -a -q)');
             }
         }
 
-        if (!$removeAll && $removeVolumes) {
+        if ($removeAll || $removeVolumes) {
             $outputString = $this->runCommand('docker volume ls -q', null, [], true);
             if (!empty($outputString)) {
                 $this->runCommand('docker volume rm $(docker volume ls -q)');
